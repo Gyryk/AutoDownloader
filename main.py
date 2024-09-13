a29eed57df3585fccd2ced4bde81419f691c7ecc
Girik <68961075+Gyryk@users.noreply.github.com>
2024-09-13 23:16:58 +0100

    Fix shows not found

--- main.py
+++ main.py
@@ -2,19 +2,32 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options 
 import transmissionrpc
 import json
 import time
+import os
 
 links = []
 
+# Construct the full path to shows.json
+script_dir = os.path.dirname(os.path.realpath(__file__))
+json_file_path = os.path.join(script_dir, 'shows.json')
+
 def fetch_magnet_link(show_name, season, episode):
-    driver = webdriver.Safari()
-    search_url = f"https://notapiracywebsite.org/search.php?q={show_name}+s{season}e{episode}&video=on&search=Pirate+Search&page=0&orderby="
+    # Set up Chrome options for headless mode
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Run in headless mode
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    # Initialize the Chrome WebDriver with headless options
+    driver = webdriver.Chrome(options=chrome_options)
+    search_url = f"https://thepiratebay.org/search.php?q={show_name}+s{season}e{episode}&video=on&search=Pirate+Search&page=0&orderby="
     
     # Open the search page
     driver.get(search_url)
-    time.sleep(4) 
+    time.sleep(5) 
 
     # Find and get the 'Magnet Download' link
     magnet_button = driver.find_element(By.XPATH, "//a[contains(@href, 'magnet:')]")
@@ -39,7 +52,7 @@ def process_name(name):
 
 def process_shows():
     # Read the shows.json file
-    with open('shows.json', 'r') as f:
+    with open(json_file_path, 'r') as f:
         shows = json.load(f)
 
     # Iterate through the shows and fetch magnet links
@@ -74,8 +87,9 @@ def process_shows():
 process_shows()
 
 # Add all the torrents to transmission
-for magnet_link in links:
-    tc = transmissionrpc.Client('localhost', port=9091)  
-    tc.add_torrent(magnet_link)
-    print(f"Magnet link added to Transmission for downloading.")
+if len(links) > 0:
+    for magnet_link in links:
+        tc = transmissionrpc.Client('localhost', port=9091)  
+        tc.add_torrent(magnet_link)
+        print(f"Magnet link added to Transmission for downloading.")
 
